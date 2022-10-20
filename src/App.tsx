5e2c932d8961ed19c14ae52af52425d67990cbb1
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-20 03:24:08 +0300

    get isAuth in components from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { Layout } from 'antd';
-import { FC, useRef } from 'react';
+import { FC } from 'react';
 import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/Navbar';
+import { useTypedSelector } from './hooks/useTypedSelector';
 import { privateRoutes, publicRoutes } from './router';
+import { RootState } from './store';
 
 const App: FC = () => {
-  const isAuth: boolean = true;
+  const { isAuth } = useTypedSelector((state: RootState) => state.auth);
+
   const routes: RouteObject[] = isAuth ? privateRoutes : publicRoutes;
-  const router = useRef(createBrowserRouter(routes));
+  const router = createBrowserRouter(routes);
 
   return (
     <Layout>
       <Navbar />
       <Layout.Content>
-        <RouterProvider router={router.current} />
+        <RouterProvider router={router} />
       </Layout.Content>
     </Layout>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,18 @@ import { MenuClickEventHandler, MenuInfo } from 'rc-menu/lib/interface';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useTypedSelector } from '../hooks/useTypedSelector';
+
 enum MenuKey {
   LOG_IN = 'LOG_IN',
   LOG_OUT = 'LOG_OUT',
 }
 const MENU_ITEMS_PUBLIC: ItemType[] = [{ label: 'Log in', key: MenuKey.LOG_IN }];
-const MENU_ITEMS_PRIVATE: ItemType[] = [{ label: 'Log out', key: MenuKey.LOG_IN }];
+const MENU_ITEMS_PRIVATE: ItemType[] = [{ label: 'Log out', key: MenuKey.LOG_OUT }];
 
 const Navbar: FC = () => {
   //   const navigate = useNavigate();
-  //   TODO: remove
-  const isAuth: boolean = true;
+  const { isAuth } = useTypedSelector((state) => state.auth);
   const menuItems: ItemType[] = isAuth ? MENU_ITEMS_PRIVATE : MENU_ITEMS_PUBLIC;
 
   const onMenuClick: MenuClickEventHandler = (info: MenuInfo) => {
