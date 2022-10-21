d9732c45b826fa25a08dde6d0a97a278d70b5148
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-22 01:30:26 +0300

    implement auto login on app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { Layout } from 'antd';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
+import { useActions } from './hooks/useActions';
+import { User } from './models/User';
 
 const App: FC = () => {
+  const { setUser, setIsAuth } = useActions();
+
+  useEffect(() => {
+    const isAuth: string | null = localStorage.getItem('auth');
+    const username: string | null = localStorage.getItem('username');
+
+    if (isAuth) {
+      setUser({ username: username } as User);
+      setIsAuth(true);
+    }
+  }, []);
+
   return (
     <Layout>
       <BrowserRouter>
